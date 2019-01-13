058342847c9c05b5d4ccf2d4ba488ce41dc08242
Ben Halverson <benhalverson33@gmail.com>
2019-01-13 15:44:47 -0800

    moved and renamed config.js to dev.js

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  DB_URI: 'mongodb://localhost:27017/airbnb-clone',
+  SECRET: 'secret',
+  AWS_ACCESS_KEY_ID: '',
+  AWS_SECRET_ACCESS_KEY: '',
+  STRIPE_SK: ''
+}
--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./prod');
 } else {
-  module.export = require('../config');
+  module.export = require('./dev');
 }
