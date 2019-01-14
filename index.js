411a126a143e283468f6bece661571f457528f98
Ben Halverson <benhalverson33@gmail.com>
2019-01-13 16:14:10 -0800

    config issue fixed, added new TODO routes

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const config = require('./config');
-const Rental = require('./models/rental');
-const User = require('./models/user');
 const rentalRoutes = require('./routes/rentals');
 const userRoutes = require('./routes/users');
 const FakeDb = require('./fake-db');
 const app = express();
 mongoose.Promise = Promise;  
-mongoose.connect('mongodb://localhost:27017/airbnb-clone')
+mongoose.connect(config.DB_URI)
   .then(() => {
     const fakeDB = new FakeDb();
     fakeDB.seedDB();
@@ -17,8 +15,15 @@ mongoose.connect('mongodb://localhost:27017/airbnb-clone')
   .catch(e => console.error(`Error ${e}`));
 
 app.use(bodyParser.json());
+
+// API Routes
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/rentals', rentalRoutes);
+
+// TODO
+// app.use('/api/v1/bookings', bookingRoutes);
+// app.use('/api/v1/payments', paymentRoutes);
+// app.use('/api/v1', imageUploadRoutes);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   `listening on ${PORT}`;
